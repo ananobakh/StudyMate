431a845e23df9e37433056df3f421d06dbf6640e
giorgi gelashvili <gigela19@freeuni.edu.ge>
2025-06-25 23:40:29 +0400

    haha

--- StudyMate/src/main/java/module-info.java
+++ StudyMate/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.studymate {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.j;
+
 
 
     opens com.example.studymate to javafx.fxml;
